a654f37df49c9adc5e0181738752498674103b5e
Raul Araujo Machado <raularaujomachado@gmail.com>
2023-06-06 12:06:39 -0300

    add produto component

--- consumo-api-ranek/src/App.jsx
+++ consumo-api-ranek/src/App.jsx
@@ -1,10 +1,27 @@
 import { useState } from 'react'
+import Produto from './produto'
 import './App.css'
 
 function App() {
+  const [dados, setDados] = useState(null);
+  const [carregando, setCarregando] = useState(null);
+
+  async function handleClick(event) {
+    setCarregando(true);
+    const res = await fetch(`https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`);
+    const json = await res.json();
+    setDados(json);
+    setCarregando(false);
+  }
 
   return (
-    <p>teste</p>
+    <div>
+      <button onClick={handleClick}>notebook</button>
+      <button onClick={handleClick}>smartphone</button>
+      <button onClick={handleClick}>tablet</button>
+      {carregando && <p>Carregando...</p>}
+      {!carregando && dados && <Produto dados={dados}/>}
+    </div>
   )
 
 }
